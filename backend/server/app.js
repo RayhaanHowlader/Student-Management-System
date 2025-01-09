fc64955f7bec15958cd01c4474d0f0845f6c8bb1
Rayhaan Howlader <rayhaanhowlader4@gmail.com>
2025-01-09 12:05:30 +0530

    updated

--- backend/server/app.js
+++ backend/server/app.js
@@ -1,9 +1,14 @@
 const express =  require("express");
 const mongoose = require("mongoose");
 const cors=require("cors");
-
+const jwt = require('jsonwebtoken');
+const SECRET_KEY = "hello_world";
+const profileModel=require("./models/profile.js")
 const formModel=require("./models/formdetails")
+const WalletModel=require("./models/wallet.js")
 const multer=require("multer");
+const path = require("path");
+
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -23,15 +28,24 @@ const app=express();
 const port =3000;
 app.use(express.json());
 app.use(cors());
+app.use("/files", express.static(path.join(__dirname, "files")));
 
 mongoose.connect("mongodb://localhost:27017/employee");
 app.post('/reg',(req,res) => {
     EmployeeModel.create(req.body)
     .then(employees=>res.json(employees))
     .catch(err=>res.json())
+});
+app.post("",(req,res)=>{
+      
 })
 app.post('/result',(req,res) => {
     ResultsModel.create(req.body)
+    .then(results=>res.json(results))
+    .catch(err=>res.json())
+})
+app.post('/refillwallet',(req,res) => {
+    WalletModel.create(req.body)
     .then(results=>res.json(results))
     .catch(err=>res.json())
 })
@@ -39,7 +53,18 @@ app.post('/result',(req,res) => {
 app.get("/results", (req, res) => {
     ResultsModel.find()
         .then(results => {
-            res.json(results);  // Send results as a JSON response
+            res.json(results);  
+        })
+        .catch(err => {
+            res.status(500).json({ message: "Error fetching results", error: err });
+        });
+});
+
+app.get("/wallet", (req, res) => {
+    WalletModel.find()
+        .then(results => {
+            res.json(results); 
+    
         })
         .catch(err => {
             res.status(500).json({ message: "Error fetching results", error: err });
@@ -48,12 +73,13 @@ app.get("/results", (req, res) => {
 app.get("/admindetails", (req, res) => {
     FormModel.find()
         .then(results => {
-            res.json(results);  // Send results as a JSON response
+            res.json(results);  
         })
         .catch(err => {
             res.status(500).json({ message: "Error fetching results", error: err });
         });
 });
+
 app.patch("/update-verified/:adhaarNumber", (req, res) => {
     const { adhaarNumber } = req.params;
 
@@ -73,13 +99,42 @@ app.patch("/update-verified/:adhaarNumber", (req, res) => {
         });
 });
 
+const ensureAuthentication=(req,res,next)=>{
+    const auth=req.headers["authorization"];
+    if(!auth){
+        return res.status(403)
+        .json({message:"Unauthorized JWT Token"})
+    }
+    try{
+    const decoded=jwt.verify(auth,"hello_world");
+    req.user=decoded;
+    next();
+    }
+    catch(err){
+        return res.status(403)
+        .json({message:"Unauthorized JWT Token wrong or expired"});
+    }
+}
+
+app.get("/dash",ensureAuthentication,(req,res)=>{
+    console.log(req.user);
+})
+
 app.post("/login",(req,res)=>{
     const {email,password} = req.body;
     EmployeeModel.findOne({email:email})
     .then(user=>{
         if(user){
             if(user.password==password){
-                res.json("Success");
+                const token = jwt.sign({ id: user._id, email: user.email }, SECRET_KEY, { expiresIn: '24h' });
+                console.log(token);
+                res.json({ 
+                    success:true,
+                    message:"Login Successfull",
+                    email:user.email,
+                    jwttoken:token 
+                });
+  
             }
             else{
                 res.json("the password is incorrect");
@@ -104,10 +159,9 @@ app.post(
         if (!req.files) {
             return res.status(400).json({ message: "No files uploaded" });
         }
-
+        console.log(req.body);
         const { name, age, adhaarNumber, address, date } = req.body;
 
-        // Extract filenames from the uploaded files
         const fileData = {
             verified:false,
             adhaar: req.files.adhaar ? req.files.adhaar[0].filename : null,
@@ -131,35 +185,23 @@ app.post(
     }
 );
 
+app.post("/profile-update",upload.single("profile"),(req,res)=>{
+    if (!req.file) {
+        return res.status(400).json({ message: "No files uploaded" });
+    }
+    const profileData = {
+        profile: req.file ? req.file.filename : null,
+        ...req.body
+      }
+      console.log(req.file);
+      console.log(profileData);
+      profileModel.create(profileData)
+      .then((form) => res.status(200).json({ message: "Form submitted successfully", form }))
+      .catch((err) => res.status(500).json({ message: "Error saving form", error: err }));
 
+    
+})
 
-// app.post(
-//     "/upload-files",
-//     upload.fields([
-//         { name: "adhaar", maxCount: 1 },
-//         { name: "twelve", maxCount: 1 },
-//         { name: "eleventh", maxCount: 1 },
-//     ]),
-//     (req, res) => {
-//         console.log(req.body);
-//         console.log("Adhaar:", req.files.adhaar);
-//         console.log("12th Marksheet:", req.files.twelve);
-//         console.log("10th Marksheet:", req.files.eleventh);
-
-//         if (!req.files) {
-//             return res.status(400).send("No files uploaded");
-//         }
-
-//         res.status(200).json({
-//             message: "Files uploaded successfully",
-//             files: req.files,
-//         });
-//         formModel.create(req.body)
-//         .then(employees=>res.json(employees))
-//         .catch(err=>res.json())
-//     }
-
-// );
 
 
 app.get("/",(req,res)=>{
@@ -172,9 +214,44 @@ app.post("/checkadmin",(req,res)=>{
         res.json("success");
     }
     else{
-        res.json("hello world");
+        res.json("invalid");
     }
 });
+
+
+
+app.get("/wallet/:email", (req, res) => {
+    const { email } = req.params;
+    console.log(email);
+    WalletModel.findOne({ email: email })
+      .then((profile) => {
+        if (!profile) {
+          return res.status(404).json({ message: "Profile not found" });
+        }
+        res.json({
+           money:profile.money// Serve the image using a relative path
+        });
+      })
+      .catch((err) => res.status(500).json({ message: "Error fetching profile", error: err }));
+  });
+
+app.get("/profile/:email", (req, res) => {
+    const { email } = req.params;
+    console.log(email);
+    profileModel.findOne({ email: email })
+      .then((profile) => {
+        if (!profile) {
+          return res.status(404).json({ message: "Profile not found" });
+        }
+        res.json({
+          firstname: profile.firstname,
+          lastname: profile.lastname,
+          profileImage: `/files/${profile.profile}` // Serve the image using a relative path
+        });
+      })
+      .catch((err) => res.status(500).json({ message: "Error fetching profile", error: err }));
+  });
+
 app.listen(port,()=>{
     console.log(`server is running at port ${port}`);
 });
--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,23 +1,32 @@
-import { Button } from "./components/ui/button.jsx"
-import styles from './login.module.css'
 import {useState} from "react"
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 function Login(){
-   
+    const [isInvalid, setIsInvalid] = useState(false); // State to track invalid credentials
+
    const [name,setName] = useState();
    const [email,setEmail] = useState();
    const [password,setPassword] = useState();
    const navigate=useNavigate();
-    const [isInvalid, setIsInvalid] = useState(false); // State to track invalid credentials
-   
+ 
 
    const handleSubmit=(e)=>{
     e.preventDefault();
     axios.post('http://localhost:3000/login',{email,password}).then(result=>{
-    console.log(result); 
-    navigate("/dashboard");
+        console.log(result.data);
+        const { success,message,email,jwttoken }=result.data; 
+        if(success){
+
+            localStorage.setItem("token",jwttoken);
+            localStorage.setItem("LoggedInUser",email); 
+            navigate("/dash")         
+        }
+        else{
+            setIsInvalid(true);
+        }
+       
+     
     })
     .catch(err=>console.log(err))
 
@@ -27,13 +36,37 @@ function Login(){
 
 
     return(
-        <div id={styles.container}>
-    <form method="post" id={styles.cont} onSubmit={handleSubmit}>
-        <h1 className={styles.heading}><svg xmlns="http://www.w3.org/2000/svg" height="45px" viewBox="0 -960 960 960" width="45px" fill="#FFFFFF"><path d="M480-120 200-272v-240L40-600l440-240 440 240v320h-80v-276l-80 44v240L480-120Zm0-332 274-148-274-148-274 148 274 148Zm0 241 200-108v-151L480-360 280-470v151l200 108Zm0-241Zm0 90Zm0 0Z"/></svg>Student Login</h1>
-        <span><label className={styles.label} htmlFor="email"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm320-280L160-640v400h640v-400L480-440Zm0-80 320-200H160l320 200ZM160-640v-80 480-400Z"/></svg>Enter Your Email : </label><input className={styles.input} type="email" id="email" name="email"  placeholder="Enter Your email address" onChange={(e)=>setEmail(e.target.value)}/></span>
-        <span><label className={styles.label} htmlFor="password"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M240-80q-33 0-56.5-23.5T160-160v-400q0-33 23.5-56.5T240-640h40v-80q0-83 58.5-141.5T480-920q83 0 141.5 58.5T680-720v80h40q33 0 56.5 23.5T800-560v400q0 33-23.5 56.5T720-80H240Zm0-80h480v-400H240v400Zm240-120q33 0 56.5-23.5T560-360q0-33-23.5-56.5T480-440q-33 0-56.5 23.5T400-360q0 33 23.5 56.5T480-280ZM360-640h240v-80q0-50-35-85t-85-35q-50 0-85 35t-35 85v80ZM240-160v-400 400Z"/></svg>Enter Your Password</label><input className={styles.input} type="password"  placeholder="Enter Your Password" onChange={(e)=>setPassword(e.target.value)}/></span>
-        <input className={styles.submit} type="submit" value="Submit" />
+        <div className="flex  items-center justify-center min-h-screen  bg-gray-950 px-4">
+     <form method="post" onSubmit={handleSubmit} className="bg-gradient-to-b from-gray-800 to-gray-700 p-6 sm:p-8 rounded-lg shadow-md w-full max-w-sm sm:max-w-md space-y-6">
+        <h1 className="flex items-center text-white text-xl sm:text-2xl  font-bold mb-4 ">
+        <svg xmlns="http://www.w3.org/2000/svg" height="35px" viewBox="0 -960 960 960" width="35px" fill="white" className="mr-2">
+        <path d="M480-120 200-272v-240L40-600l440-240 440 240v320h-80v-276l-80 44v240L480-120Zm0-332 274-148-274-148-274 148 274 148Zm0 241 200-108v-151L480-360 280-470v151l200 108Zm0-241Zm0 90Zm0 0Z"/>
+        </svg>
+        Student Login
+        </h1>
+        <div className="text-white "
+                       style={{ display: isInvalid ? "block" : "none" }}
+                   >Invalid username or password</div>
+            <div className="space-y-2">
+            <label htmlFor="email" className="flex items-center text-white font-medium"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="white" className="mr-2"><path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm320-280L160-640v400h640v-400L480-440Zm0-80 320-200H160l320 200ZM160-640v-80 480-400Z"/>
+            </svg>Enter Your Email : </label>
+            <input type="email" id="email" className="w-full px-4 py-2 rounded-lg bg-gray-800 text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-yellow-500" name="email"  placeholder="Enter Your email address" onChange={(e)=>setEmail(e.target.value)}/>
+            </div>
+
+
+
+            <div className="space-y-2">
+            <label htmlFor="password" className="flex items-center text-white font-medium"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="white" className="mr-2"><path d="M240-80q-33 0-56.5-23.5T160-160v-400q0-33 23.5-56.5T240-640h40v-80q0-83 58.5-141.5T480-920q83 0 141.5 58.5T680-720v80h40q33 0 56.5 23.5T800-560v400q0 33-23.5 56.5T720-80H240Zm0-80h480v-400H240v400Zm240-120q33 0 56.5-23.5T560-360q0-33-23.5-56.5T480-440q-33 0-56.5 23.5T400-360q0 33 23.5 56.5T480-280ZM360-640h240v-80q0-50-35-85t-85-35q-50 0-85 35t-35 85v80ZM240-160v-400 400Z"/>
+            </svg>Enter Your Password :</label>
+            <input type="password" id="password" className="w-full px-4 py-2 rounded-lg bg-gray-800 text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-yellow-500"  placeholder="Enter Your Password" onChange={(e)=>setPassword(e.target.value)}/>
+            </div>
+        
+        <button type="submit" value="Submit"
+        className="w-full py-2 bg-green-500 text-white text-xl font-bold rounded-lg hover:bg-green-600 transition">
+            Log In   
+        </button>
+    
     </form>
     </div>);
 }
-export default Login
+export default Login
