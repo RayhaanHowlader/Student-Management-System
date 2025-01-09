4fec99f54bb658d8f0220573610c3ca8d272d17c
Rayhaan Howlader <rayhaanhowlader4@gmail.com>
2025-01-09 11:59:21 +0530

    modified

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,25 @@
 
 import React from "react";
+import Profile from "./profile.jsx"
 import Verify from "./adminverify.jsx"
+import ATKT from "./atkt.jsx"
+import Refill from "./refillwallet.jsx"
+import Payment from './payment.jsx'
 import Admission from "./admissionform.jsx"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Login.jsx";
 import Register from "./Register.jsx"; 
+import Wallet from "./wallet.jsx"
 import './App.css';
+import WalletUser from './walletuser.jsx'
 import Dashboard from "./dashboard.jsx";
 import Admin from './admin.jsx'
 import HomePage from "./pages/HomePage.jsx"
 import Results from './resultsform.jsx'
 import AboutPage  from "./pages/AboutPage.jsx";
 import ResultsPage from './resultpage.jsx'
+import { Navigate } from 'react-router-dom';
+
 function App() {
   return (
     <BrowserRouter>
@@ -26,7 +34,13 @@ function App() {
         <Route path="/admissionform" element={<Admission/>}/>
         <Route path="/adminverify" element={<Verify/>}/>
         <Route path="/about" element={<AboutPage/>}/>
+        <Route path="/payment" element={<Payment/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/walletadmin" element={<Refill/>}/>
         <Route path="/" element={<HomePage/>}/>
+        <Route path="/wallet" element={<Wallet></Wallet>}></Route>
+        <Route path="/walletuser" element={<WalletUser></WalletUser>}></Route>
+        <Route path="/atkt" element={<ATKT></ATKT>}> </Route>
         </Routes>
     </BrowserRouter>
   );
